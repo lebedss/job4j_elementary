4698348867b36163117455877ccf72fe6d9aedf7
Sergey Lebedev <lebedevss88@gmail.com>
2023-04-22 01:14:34 +0300

    Тернарное сравнение [#189 #144090] Test updated using JUnit5

--- test/ru/job4j/condition/MaxTest.java
+++ test/ru/job4j/condition/MaxTest.java
@@ -1,13 +1,15 @@
 package ru.job4j.condition;
 
-import org.junit.Test;
-import static org.hamcrest.core.Is.is;
-import static org.junit.Assert.assertThat;
+import org.junit.jupiter.api.Test;
+import static org.assertj.core.api.Assertions.assertThat;
 
 public class MaxTest {
     @Test
     public void whenMax1To2Then2() {
-        int result = Max.max(1, 2);
-        assertThat(result, is(2));
+        int left = 1;
+        int right = 2;
+        int result = Max.max(left, right);
+        int expected = 2;
+        assertThat(result).isEqualTo(expected);
     }
 }
