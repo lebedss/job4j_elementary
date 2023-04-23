47b9e485c94474dfd3624fa57ccca602afca7573
Sergey Lebedev <lebedevss88@gmail.com>
2023-04-23 23:31:45 +0300

    Сумма чисел. [#226416 #144146]

--- src/ru/job4j/loop/Counter.java
+++ src/ru/job4j/loop/Counter.java
@@ -1,5 +1,6 @@
 package ru.job4j.loop;
- public class Counter {
+
+public class Counter {
     public static int sum(int start, int finish) {
         int sum = 0;
         for (int value = start; value <= finish; value++) {
@@ -9,12 +10,18 @@ package ru.job4j.loop;
     }
 
     public static int sumByEven(int start, int finish) {
-         int sumEven = 0;
-         for (int value = start; value <= finish; value++) {
-             if (value % 2 == 0) {
-                 sumEven = sumEven + value;
-             }
-         }
-         return sumEven;
-     }
+        int sumEven = 0;
+        for (int value = start; value <= finish; value++) {
+            if (value % 2 == 0) {
+                sumEven = sumEven + value;
+            }
+        }
+        return sumEven;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(sum(0, 10));
+        System.out.println(sum(3, 8));
+        System.out.println(sum(1, 1));
+    }
 }
