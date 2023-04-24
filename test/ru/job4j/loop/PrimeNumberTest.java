e36e9b318bf9fb4b4f0a9d22f6cea96b589e8813
Sergey Lebedev <lebedevss88@gmail.com>
2023-04-24 22:09:34 +0300

    Простые числа [#156314 #144144] test updated

--- test/ru/job4j/loop/PrimeNumberTest.java
+++ test/ru/job4j/loop/PrimeNumberTest.java
@@ -1,25 +1,31 @@
 package ru.job4j.loop;
-import org.junit.Test;
 
-import static org.hamcrest.Matchers.is;
-import static org.junit.Assert.*;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 public class PrimeNumberTest {
     @Test
     public void when5() {
-        int count = PrimeNumber.calc(5);
-        assertThat(count, is(3));
+        int finish = 5;
+        int result = PrimeNumber.calc(finish);
+        int expected = 3;
+        assertThat(result).isEqualTo(expected);
     }
 
     @Test
     public void when11() {
-        int count = PrimeNumber.calc(11);
-        assertThat(count, is(5));
+        int finish = 11;
+        int result = PrimeNumber.calc(finish);
+        int expected = 5;
+        assertThat(result).isEqualTo(expected);
     }
 
     @Test
     public void when2() {
-        int count = PrimeNumber.calc(2);
-        assertThat(count, is(1));
+        int finish = 2;
+        int result = PrimeNumber.calc(finish);
+        int expected = 1;
+        assertThat(result).isEqualTo(expected);
     }
 }
