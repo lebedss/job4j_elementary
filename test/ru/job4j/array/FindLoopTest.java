dd4bee0f9c48422dfc680534025d6a76bb11d513
Sergey Lebedev <lebedevss88@gmail.com>
2023-05-15 01:26:39 +0300

    6.1. Классический поиск перебором. [#33489 #144049]

--- test/ru/job4j/array/FindLoopTest.java
+++ test/ru/job4j/array/FindLoopTest.java
@@ -1,48 +1,25 @@
 package ru.job4j.array;
 
-import org.junit.Test;
-import static org.hamcrest.core.Is.is;
-import static org.junit.Assert.assertThat;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 public class FindLoopTest {
-
     @Test
-
     public void whenArrayHas5Then0() {
-        int[] input = {5, 4, 3, 2};
-        int value = 5;
-        int result = FindLoop.indexOf(input, value);
-        int expect = 0;
-        assertThat(result, is(expect));
+        int[] data = new int[]{5, 10, 3};
+        int el = 5;
+        int result = FindLoop.indexOf(data, el);
+        int expected = 0;
+        assertThat(result).isEqualTo(expected);
     }
 
     @Test
-
-    public void whenArrayDoesNotContainElement() {
-        int[] input = {5, 4, 3, 2};
-        int value = 6;
-        int result = FindLoop.indexOf(input, value);
-        int expect = -1;
-        assertThat(result, is(expect));
-    }
-
-    @Test
-    public void whenArrayHasLength5Then0() {
-        int[] input = new int[] {5, 10, 3};
-        int value = 5;
-        int result = FindLoop.indexOf(input, value);
-        int expect = 0;
-        assertThat(result, is(expect));
-    }
-
-    @Test
-    public void whenFind3() {
-        int[] input = new int[] {5, 2, 10, 2, 4};
-        int value = 2;
-        int start = 2;
-        int finish = 4;
-        int result = FindLoop.indexOf(input, value, start, finish);
-        int expect = 3;
-        assertThat(result, is(expect));
+    public void whenArrayHasNot10ThenMinus1() {
+        int[] data = new int[]{5, 10, 3};
+        int el = 11;
+        int result = FindLoop.indexOf(data, el);
+        int expected = -1;
+        assertThat(result).isEqualTo(expected);
     }
 }
