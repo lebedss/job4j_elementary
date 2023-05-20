2dc4bc606672184055fd83d32687ceb84ec8f57a
Sergey Lebedev <lebedevss88@gmail.com>
2023-05-21 01:06:21 +0300

    6.5.2. Поиск минимума в диапазоне. [#179804 #144060]  Test updated

--- test/ru/job4j/array/MinDiapasonTest.java
+++ test/ru/job4j/array/MinDiapasonTest.java
@@ -1,42 +1,36 @@
 package ru.job4j.array;
 
-import org.junit.Test;
-
-import static org.hamcrest.core.Is.is;
-import static org.junit.Assert.*;
+import org.junit.jupiter.api.Test;
+import static org.assertj.core.api.Assertions.assertThat;
 
 public class MinDiapasonTest {
-
     @Test
     public void whenFirstMin() {
-        assertThat(
-                MinDiapason.findMin(
-                        new int[] {-1, 0, 5, 10},
-                        1, 3
-                ),
-                is(0)
-        );
+        int[] array = new int[] {-1, 0, 5, 10};
+        int start = 1;
+        int finish = 3;
+        int result = MinDiapason.findMin(array, start, finish);
+        int expected = 0;
+        assertThat(result).isEqualTo(expected);
     }
 
     @Test
     public void whenLastMin() {
-        assertThat(
-                MinDiapason.findMin(
-                        new int[] {10, 5, 3, 1},
-                        1, 3
-                ),
-                is(1)
-        );
+        int[] array = new int[] {10, 5, 3, 1};
+        int start = 1;
+        int finish = 3;
+        int result = MinDiapason.findMin(array, start, finish);
+        int expected = 1;
+        assertThat(result).isEqualTo(expected);
     }
 
     @Test
     public void whenMiddleMin() {
-        assertThat(
-                MinDiapason.findMin(
-                        new int[] {10, 2, 5, 1},
-                        0, 2
-                ),
-                is(2)
-        );
+        int[] array = new int[] {10, 2, 5, 1};
+        int start = 0;
+        int finish = 2;
+        int result = MinDiapason.findMin(array, start, finish);
+        int expected = 2;
+        assertThat(result).isEqualTo(expected);
     }
 }
