02506f0562e12ebd3230173a6777535e67b1b3f6
Sergey Lebedev <lebedevss88@gmail.com>
2023-05-21 00:16:31 +0300

    6.5. Поиск индекса в диапазоне. [#156318 #144055] method renamed

--- src/ru/job4j/array/SortSelected.java
+++ src/ru/job4j/array/SortSelected.java
@@ -6,7 +6,7 @@ public class SortSelected {
         for (int i = 0; i < data.length; i++) {
             int min = MinDiapason.findMin(data, i, data.length - 1);
             /* минимальный элемент в массиве*/
-            int index = FindLoop.indexOf(data, min, i, data.length - 1);
+            int index = FindLoop.indexInRange(data, min, i, data.length - 1);
             /* индекс минимального элемента*/
             int tmp = data[index];
             data[index] = data[i];
