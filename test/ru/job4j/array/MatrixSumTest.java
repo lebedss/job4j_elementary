59d41f687e500b0953c61b4820554d9bc968245e
Sergey Lebedev <lebedevss88@gmail.com>
2023-05-22 00:04:28 +0300

    6.6.2. вухмерный массив. иклы. [#306725 #144073] Test updated

--- test/ru/job4j/array/MatrixSumTest.java
+++ test/ru/job4j/array/MatrixSumTest.java
@@ -1,41 +1,39 @@
 package ru.job4j.array;
 
-import static org.junit.Assert.assertThat;
-import static org.hamcrest.Matchers.is;
+import org.junit.jupiter.api.Test;
 
-import org.junit.Test;
+import static org.assertj.core.api.Assertions.assertThat;
 
 public class MatrixSumTest {
-
     @Test
     public void whenSingle() {
-        int[][] in = {
+        int[][] array = {
                 {10}
         };
-        int expect = 10;
-        int rsl = MatrixSum.sum(in);
-        assertThat(rsl, is(expect));
+        int result = MatrixSum.sum(array);
+        int expected = 10;
+        assertThat(result).isEqualTo(expected);
     }
 
     @Test
     public void whenTwo() {
-        int[][] in = {
+        int[][] array = {
                 {1, 2},
                 {1, 2}
         };
-        int expect = 6;
-        int rsl = MatrixSum.sum(in);
-        assertThat(rsl, is(expect));
+        int result = MatrixSum.sum(array);
+        int expected = 6;
+        assertThat(result).isEqualTo(expected);
     }
 
     @Test
     public void whenThree() {
-        int[][] in = {
+        int[][] array = {
                 {1, 2, 1},
                 {1, 2, 0}
         };
-        int expect = 7;
-        int rsl = MatrixSum.sum(in);
-        assertThat(rsl, is(expect));
+        int result = MatrixSum.sum(array);
+        int expected = 7;
+        assertThat(result).isEqualTo(expected);
     }
 }
