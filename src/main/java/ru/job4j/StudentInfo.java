b8ec251b87cfe5afebcfdcf090b4e2a70ed6528f
Sergey Lebedev <lebedevss88@gmail.com>
2025-07-14 21:47:10 +0300

    4.2. Заголовок в файле java [#159036 #144120]

--- src/main/java/ru/job4j/StudentInfo.java
+++ src/main/java/ru/job4j/StudentInfo.java
@@ -1,4 +1,8 @@
 package ru.job4j;
 
 public class StudentInfo {
+    public static void main(String[] args) {
+        System.out.println("Sergey Lebedev");
+        System.out.println("25.05.1988");
+    }
 }
